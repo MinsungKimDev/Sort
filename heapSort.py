af15876fd1b1fcf6177d955cbc33bdeb18392d83
MinsungKimDev <minsungkim.dev@gmail.com>
2024-01-09 23:22:28 +0900

    Add comment to heapSort.py

--- heapSort.py
+++ heapSort.py
@@ -2,6 +2,17 @@
 # 파이썬 힙정렬 알고리즘 
 # 시간복잡도 최선: O(nlog2n) 평균: O(nlog2n) 최악: O(nlog2n)
 
+arr = []
+
+# 기본 입력 로직 --------------
+print("Please enter the number array : ", end="")
+str = input() # 문자열 입력받기
+str = str.split() # 공백 기준으로 문자열 자르기
+for item in str:
+    arr.append(int(item)) # 각각의 요소를 정수형으로 변환후 리스트에 추가
+# ----------------------------
+
+# 알고리즘 로직 ---------------
 def heapify(arr, index, heap_size):
     largest = index
     left = 2 * index + 1
@@ -26,3 +37,7 @@ def heapSort(arr):
         heapify(arr, 0, i)
 
     return arr
+# ----------------------------
+
+arr = heapSort(arr)
+print(arr)
